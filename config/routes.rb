6e8a630a2e41840d3d88e3aa21c767fe7924212e
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-11 16:41:47 +0900

    Add user_controller: index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index, :show]
   resources :books
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?

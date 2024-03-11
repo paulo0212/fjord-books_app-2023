df5b29bcec8b194abf2ba395d289bc5ecde9c7f8
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-11 18:26:14 +0900

    Add profiles to users

---
+++ db/migrate/20240311075517_add_profiles_to_users.rb
@@ -0,0 +1,8 @@
+class AddProfilesToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :zip_code, :string
+    add_column :users, :address, :string
+    add_column :users, :biography, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_11_050406) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_11_075517) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,6 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_11_050406) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "zip_code"
+    t.string "address"
+    t.text "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

6b95a37cd769327451c5ddd91b6b1bf99526fb69
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-13 17:36:33 +0900

    Modify view files for i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  around_action :switch_locale, default_url_options
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end

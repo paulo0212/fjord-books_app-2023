5a9334bfb713514415d570af196dbd360d74933a
paulo0212 <rikuo.iwasaki@gmail.com>
2024-04-04 17:12:11 +0900

    Implement add mention feature

--- app/models/report.rb
+++ app/models/report.rb
@@ -11,11 +11,29 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_create do
+    mentioned_report_ids = extract_mentioning_report_ids
+    add_mentions(mentioned_report_ids)
+  end
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  def add_mentions(mentioned_report_ids)
+    mentioned_report_ids.each do |mentioned_report_id|
+      active_mentions.create(mentioned_report_id:)
+    end
+  end
+
+  private
+
+  def extract_mentioning_report_ids
+    regexp = %r{http://localhost:3000/reports/(?<report_id>\d+)}
+    content.scan(regexp).flatten.uniq.map(&:to_i)
   end
 end

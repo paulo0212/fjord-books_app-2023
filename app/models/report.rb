aa8368a6f2fb9f7a10f7492e909c2f52c6a27a62
paulo0212 <rikuo.iwasaki@gmail.com>
2024-04-04 16:35:31 +0900

    Add associations with Mention model

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mentioned_report_id', inverse_of: :mentioned_report, dependent: :destroy
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report
+  has_many :mentioned_reports, through: :passive_mentions, source: :report
 
   validates :title, presence: true
   validates :content, presence: true

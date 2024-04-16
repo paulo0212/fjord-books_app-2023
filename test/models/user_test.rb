c6c24f60e5482dc036f752c7b1dc3385d87163b0
paulo0212 <rikuo.iwasaki@gmail.com>
2024-04-16 18:04:43 +0900

    Create model test for User class

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # name_or_email
+  test 'returns name if user has name' do
+    user = FactoryBot.create(:user, name: 'TestTaro')
+    assert_equal 'TestTaro', user.name_or_email
+  end
+
+  test 'returns email if name is blank' do
+    user = FactoryBot.create(:user, name: '', email: 'testaro@example.com')
+    assert_equal 'testaro@example.com', user.name_or_email
+  end
 end
